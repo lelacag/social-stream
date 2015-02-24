12a47c55c1917e2c9a4a781ab8bc307144de738e
Alex Lyashenko <rubydeve@gmail.com>
2015-02-24 20:24:50 +0200

    annotate models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  parent_ids :text             default("[]")
+#  child_ids  :text             default("[]")
+#  owner_id   :integer
+#  owner_type :string
+#
+
 class Category < ActiveRecord::Base
 	acts_as_inkwell_category
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,10 +8,11 @@
 #  body                      :text
 #  created_at                :datetime         not null
 #  updated_at                :datetime         not null
+#  censor_approved           :boolean
 #  users_ids_who_favorite_it :text             default("[]")
 #  users_ids_who_comment_it  :text             default("[]")
 #  users_ids_who_reblog_it   :text             default("[]")
-#  censor_approved           :boolean
+#  communities_ids           :text             default("[]")
 #
 
 class Post < ActiveRecord::Base
