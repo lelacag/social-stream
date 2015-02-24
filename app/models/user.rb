64e5355a9eb16363b98cecb5ce21ccbc305d367f
Alex Lyashenko <rubydeve@gmail.com>
2015-02-24 20:13:59 +0200

    user can add avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User < ActiveRecord::Base
   has_many :post_reports
   has_many :phrases
   acts_as_inkwell_user
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "avatar.gif"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "50x50#" }, :default_url => "avatar.gif"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   
 end
