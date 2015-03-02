e3b9aa47174eea87ca8939140732df3f14a61a53
Alex Lyashenko <rubydeve@gmail.com>
2015-03-02 18:56:35 +0200

    fix redirect

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -18,8 +18,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
         expire_data_after_sign_in!
         # respond_with resource, location: after_inactive_sign_up_path_for(resource)
-        # render js: "window.location.href=' " +  community_path(1) + " ' "
-        redirect_to communities_path, notice: 'Community was successfully created.'
+        render js: "window.location.href=' " +  communities_path + " ' "
+        # render html: redirect_to communities_path, notice: 'Community was successfully created.'
       end
     else
       clean_up_passwords resource
