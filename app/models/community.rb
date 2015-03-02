9a8b25db167ef2e50b20fda86c7e419718aed7b3
Alex Lyashenko <rubydeve@gmail.com>
2015-03-02 14:07:35 +0200

    default image for community

--- app/models/community.rb
+++ app/models/community.rb
@@ -3,6 +3,6 @@ class Community < ActiveRecord::Base
   validates :name, presence: true
   # validates :name, uniqueness: true
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "assets/no_image.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
