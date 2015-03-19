0656e0e5d1fe1321683d006640c649e8233fc40a
Alex Lyashenko <rubydeve@gmail.com>
2015-03-19 18:55:00 +0200

    change style for images

--- app/models/community.rb
+++ app/models/community.rb
@@ -3,6 +3,6 @@ class Community < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: true
 
-  has_attached_file :image, styles: { medium: '300x300>', thumb: '190x225>' }, default_url: 'no_image.png'
+  has_attached_file :image, styles: { medium: '300x300>', thumb: '400x400>' }, default_url: 'no_image.png'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :post_reports
   acts_as_inkwell_user
-  has_attached_file :avatar, styles: { medium: '300x300>', thumb: '50x50#' }, default_url: 'avatar.gif'
+  has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100#' }, default_url: 'avatar.gif'
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   validates :username, presence: true
