0263f80507119560083c62b204c8782140e27056
Alex Lyashenko <rubydeve@gmail.com>
2015-03-23 19:42:36 +0200

    temp fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SocialStream::Application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  # devise_for :admin_users, ActiveAdmin::Devise.config
+  # ActiveAdmin.routes(self)
   # resources :posts do
   #   post 'like'
   #   post 'report'
