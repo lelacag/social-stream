bd0362c79a81c63dd3be130efaa6e47716d43d7a
Alex Lyashenko <rubydeve@gmail.com>
2015-03-23 18:46:19 +0200

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
-lock '3.3.5'
+lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'setphrase'
+set :repo_url, 'git@github.com:alexandrule/social-stream.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
