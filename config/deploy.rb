c1b36d1bf8eedbea9e39eb984307fe6e7c7e7f0c
Alex Lyashenko <rubydeve@gmail.com>
2015-03-28 20:29:27 +0200

    fix deploy for production

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,40 +7,12 @@ lock '3.4.0'
 set :repo_url,        'git@github.com:alexandrule/social-stream.git'
 set :application,     'setphrase'
 set :user,            'root'
-set :puma_threads,    [0, 16]
-set :puma_workers,    0
-
-# Don't change these unless you know what you're doing
-set :pty,             true
-set :use_sudo,        false
-# set :stage,           :production
-set :deploy_via,      :remote_cache
-# set :deploy_to,       "/home/apps/#{fetch(:application)}"
-set :puma_rackup,             -> { File.join(current_path, 'config.ru') }
-set :puma_state,              "#{shared_path}/tmp/pids/puma.state"
-set :puma_bind,               "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,              "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,                "#{shared_path}/tmp/pids/puma.pid"
-# set :puma_conf,               "#{shared_path}/puma.rb"
-set :puma_access_log,         "#{release_path}/log/puma.error.log"
-set :puma_error_log,          "#{release_path}/log/puma.access.log"
-set :puma_role,               :app
-set :puma_env,                fetch(:rack_env, fetch(:rails_env, 'production'))
-set :ssh_options,             { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app,        true
-set :puma_worker_timeout,     nil
-set :puma_init_active_record, true  # Change to true if using ActiveRecord
-
-## Defaults:
-# set :scm,           :git
-set :branch,        :develop
-set :format,        :pretty
-set :log_level,     :debug
-set :keep_releases, 1
+# set :puma_threads,    [0, 16]
+# set :puma_workers,    0
 
 ## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w{config/database.yml}
+set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,3 +2,24 @@ server 'setphrase.com', port: 22, roles: [:web, :app, :db], primary: true
 
 set :stage,     :production
 set :deploy_to, "/home/apps/#{fetch(:application)}"
+
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+# set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/apps/#{fetch(:application)}"
+# set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+# set :puma_state, -> { File.join(shared_path, 'tmp', 'pids', 'puma.state') }
+# set :puma_pid, -> { File.join(shared_path, 'tmp', 'pids', 'puma.pid') }
+# set :puma_bind, -> { File.join("unix://#{shared_path}", 'tmp', 'sockets', "#{fetch(:application)}-puma.sock") }
+# set :puma_conf, -> { File.join(shared_path, 'puma.rb') }
+# set :puma_access_log, -> { File.join(shared_path, 'log', 'puma_access.log') }
+# set :puma_error_log, -> { File.join(shared_path, 'log', 'puma_error.log') }
+# set :puma_role,               :app
+# set :puma_env,                fetch(:rack_env, fetch(:rails_env, 'production'))
+set :ssh_options,             { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+# set :puma_preload_app,        true
+# set :puma_prune_bundler, true
+# set :puma_worker_timeout,     nil
+set :puma_init_active_record, true  # Change to true if using ActiveRecord
